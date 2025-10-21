fe25fb1de28666a5dba6487cd83c66831831830a
Ridlofalihin <falihinredo@gmail.com>
2025-10-21 08:59:20 +0700

    awal

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Program sederhana untuk menampilkan pesan ke layar.
+ * Bagian dari latihan dokumentasi (Codelab 2)
+ *
+ * @author Nama Kamu
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di DocumentationPractice!");
+        System.out.println("Program ini dibuat untuk latihan Codelab 2 Modul 3.");
+    }
+}
